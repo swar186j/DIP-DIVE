df6349fa2387e9a8fe1b3c93f703766317742505
swar186j <74865434+swar186j@users.noreply.github.com>
2022-05-30 16:46:46 +0530

    Add files via upload

---
+++ dipdive/static/javascript/questions.js
@@ -0,0 +1,139 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In past four weeks do you have experience of numbness or tingling?",
+    answer: ["Moderate", "Severe"],
+    options: [
+      "Not at all",
+      "Mild",
+      "Moderate",
+      "Severe"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Do you experience frequent headache?",
+    answer: ["Moderate", "Severe"],
+    options: [
+      "Not at all",
+      "Mild",
+      "Moderate",
+      "Severe"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Are you unable to relax yourself?",
+    answer: ["Moderate", "Severe"],
+    options: [
+      "Not at all",
+      "Mild",
+      "Moderate",
+      "Severe"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Are you experiencing fear of worst happening?",
+    answer: ["Moderate", "Severe"],
+    options: [
+      "Not at all",
+      "Mild",
+      "Moderate",
+      "Severe"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Do you feel dizzy or lightheaded?",
+    answer: ["Moderate", "Severe"],
+    options: [
+      "Not at all",
+      "Mild",
+      "Moderate",
+      "Severe"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+     numb:6,
+    question: "Does your mind remains unsteady?",
+    answer: ["Moderate", "Severe"],
+    options: [
+        "Not at all",
+        "Mild",
+        "Moderate",
+        "Severe"
+      ]
+   },
+
+   {
+    numb:7,
+   question: "Do you ever feel nervous & hesitating while talking with others?",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  },
+  {
+    numb:8,
+   question: "Do you feel fear of losing control over yourself",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  },
+  {
+    numb:9,
+   question: "Do you ever feel scared at sudden?",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  },
+  {
+    numb:10,
+   question: "I feel lonely",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  },
+  {
+    numb:11,
+   question: "Does other's opinions put impact on you?",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  },
+  {
+    numb:12,
+   question: "Do you get fearful dreams?",
+   answer: ["Moderate", "Severe"],
+   options: [
+       "Not at all",
+       "Mild",
+       "Moderate",
+       "Severe"
+     ]
+  }
+];
